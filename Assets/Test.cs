a59bbd6464f727395913f819a943019750055e89
muzuk <muzuk71101225@gmail.com>
2023-07-29 21:22:31 +0900

    Magic関数の追加
    発展課題のため、Magic関数を追加した。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,27 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss
+{
+    private int hp = 100; //体力
+    private int power = 25; //攻撃力
+    private int mp = 53; //魔力
 
+
+
+    public void Attack()
+    {
+        //攻撃用の関数
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+    public void Defence(int damage)
+    {
+        //防御用の関数
+        Debug.Log(damage + "のダメージを受けた");
+        //残りhpを減らす
+        this.hp -= damage;
+    }
+    public void Magic()
+    {
+        for (int i = 0; i < 11; i++)
+        {
+            if (this.mp >= 5)
+            {
+                //mpを5以上の場合は5減らし、コンソールに 魔法攻撃をした。残りMPは〇〇。 と表示
+                this.mp -= 5;
+                Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
+            }
+            else
+            {
+                //mpが5未満の場合、MPが足りないため、魔法が使えない。 と表示
+                Debug.Log("MPが足りないため、魔法が使えない。");
+            }
+        }
+
+    }
+}
 
 public class Test : MonoBehaviour
 {
     //Start is called before the first frame update
     void Start()
     {
-        //要素の個数が5の、int型の配列arrayを宣言して好きな値で初期化
-        int[] array= { 0, 1, 2, 3, 4 };
+        //Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
 
-        //for文を使い、配列の各要素の値を順番に表示
-        for (int i = 0; i<array.Length; i++)
-        {
-            Debug.Log(array [i]);
-        }
-        //for文を使い、配列の各要素の値を逆順に表示
-        for (int i = 4; i<array.Length; i--)
-        {
-            Debug.Log(array[i]);
-        }
+        //攻撃用の関数を呼び出す
+        lastboss.Attack();
+        //防御用の関数を呼び出す
+        lastboss.Defence(3);
+        //魔法攻撃用の関数を呼び出す
+        lastboss.Magic();
+        
     }
+
 
 
 
